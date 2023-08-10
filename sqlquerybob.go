2e1a7d0364f1287fd5ded7beea079d0d97e57d70
Petros Sordinas <petros@sordinas.net>
2023-08-10 10:51:22 +0300

    Initial commit

---
+++ sqlquerybob.go
@@ -0,0 +1,499 @@
+package sqlquerybob
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type database int
+
+const (
+	MYSQL database = iota
+	SQLITE
+	POSTGRES
+	ORACLE
+)
+
+type queryType int
+
+const (
+	selectQry queryType = iota
+	insertQry
+	updateQry
+	deleteQry
+)
+
+const (
+	validOperators = "=/>/</>=/<=/<>/IN/BETWEEN/LIKE"
+)
+
+// Errors
+type ErrBadColumnsValuesCombo struct {
+	columnCount int
+	valueCount  int
+	msg         string
+}
+
+func NewBadColumnsValuesComboError(columnCount, valueCount int) ErrBadColumnsValuesCombo {
+	return ErrBadColumnsValuesCombo{
+		columnCount: columnCount,
+		valueCount:  valueCount,
+		msg:         fmt.Sprintf("columns count (%d) must be equal to values count (%d)", columnCount, valueCount),
+	}
+}
+
+func (e ErrBadColumnsValuesCombo) Error() string {
+	return e.msg
+}
+
+type ErrInvalidSqlOperator struct {
+	operator string
+	msg      string
+}
+
+func NewInvalidOperatorError(operator string) ErrInvalidSqlOperator {
+	return ErrInvalidSqlOperator{
+		operator: operator,
+		msg:      fmt.Sprintf("operator '%s' is an invalid SQL operator", operator),
+	}
+}
+
+func (e ErrInvalidSqlOperator) Error() string {
+	return e.msg
+}
+
+var ErrFirstCriterionIsOr = errors.New("the first criterion is an OR")
+
+type Builder struct {
+	db               database
+	placeholderCount int
+	queryType        queryType
+	table            string
+	joinTables       []struct {
+		joinType string
+		table    string
+		column   string
+		fkey     string
+	}
+	columns  []string
+	values   []interface{}
+	criteria []struct {
+		column   string
+		operator string
+		values   []interface{}
+		or       bool
+	}
+	orderBy []struct {
+		column    string
+		direction int
+	}
+	limit  uint
+	offset uint
+}
+
+// Creates a new query builder for SELECT. The table on which we are going
+// to perform the select query is passed as the tableName parameter.
+func NewSelect(tableName string) *Builder {
+	return &Builder{
+		queryType: selectQry,
+		table:     tableName,
+	}
+}
+
+// Sets the database engine the queries will be produced for
+func (qb *Builder) ForDatabase(db database) *Builder {
+	qb.db = db
+	return qb
+}
+
+// Define the table columns to be selected. Table columns can be added by their name
+// or prefixed by their table name. If a table name is not prefixed, the table that has
+// been defined in NewSelect will be prefixed. For example
+//   - NewSelect("table1").Select("column1", "table2.column5") will store the columns as
+//     table1.column1, table2.column5
+func (qb *Builder) Select(columns ...string) *Builder {
+	for _, column := range columns {
+		tableColumn := strings.Split(column, ".")
+		if len(tableColumn) == 1 {
+			qb.columns = append(qb.columns, qb.table+"."+column)
+		}
+		if len(tableColumn) == 2 {
+			qb.columns = append(qb.columns, column)
+		}
+	}
+	return qb
+}
+
+// Adds a limit and / or offset clause to the query. If offset is not required, pass 0 as the
+// offset argument. Limit and offset must be non negative integers so we avoid this error by
+// making they are uints.
+func (qb *Builder) Limit(limit, offset uint) *Builder {
+	qb.limit = limit
+	qb.offset = offset
+	return qb
+}
+
+func (qb *Builder) Set(columns ...string) *Builder {
+	qb.columns = append(qb.columns, columns...)
+	return qb
+}
+
+func (qb *Builder) To(values ...interface{}) *Builder {
+	qb.values = append(qb.values, values...)
+	return qb
+}
+
+// Define the values in which the query results will be stored. These have to be
+// pointers.
+func (qb *Builder) Into(values ...interface{}) *Builder {
+	qb.values = append(qb.values, values...)
+	return qb
+}
+
+// Define a join. Multiple joins can be added by chaining this.
+func (qb *Builder) Join(joinType, table, column, fkey string) *Builder {
+	qb.joinTables = append(
+		qb.joinTables,
+		struct {
+			joinType, table, column, fkey string
+		}{
+			joinType, table, column, fkey,
+		},
+	)
+	return qb
+}
+
+// Define the where clause of the query.
+func (qb *Builder) Where(column, operator string, values ...interface{}) *Builder {
+	qb.criteria = append(
+		qb.criteria,
+		struct {
+			column   string
+			operator string
+			values   []interface{}
+			or       bool
+		}{
+			column:   column,
+			operator: strings.ToUpper(operator),
+			values:   values,
+			or:       false,
+		},
+	)
+	return qb
+}
+
+// Define a where OR clause of the query.
+func (qb *Builder) OrWhere(column, operator string, values ...interface{}) *Builder {
+	qb.criteria = append(
+		qb.criteria,
+		struct {
+			column   string
+			operator string
+			values   []interface{}
+			or       bool
+		}{
+			column:   column,
+			operator: strings.ToUpper(operator),
+			values:   values,
+			or:       true,
+		},
+	)
+	return qb
+}
+
+// Define an ascending order on a column
+func (qb *Builder) OrderBy(column string) *Builder {
+	qb.orderBy = append(
+		qb.orderBy,
+		struct {
+			column    string
+			direction int
+		}{
+			column:    column,
+			direction: 1,
+		},
+	)
+	return qb
+}
+
+// Define a descending order on a column
+func (qb *Builder) OrderByDescending(column string) *Builder {
+	qb.orderBy = append(
+		qb.orderBy,
+		struct {
+			column    string
+			direction int
+		}{
+			column:    column,
+			direction: -1,
+		},
+	)
+	return qb
+}
+
+// Returns the pointer values in which the results will be stored
+func (qb *Builder) Values() []interface{} {
+	return qb.values
+}
+
+// Returns the criteria values that have been defined with Where
+func (qb *Builder) Criteria() []interface{} {
+	var values []interface{}
+	for _, criterion := range qb.criteria {
+		values = append(values, criterion.values...)
+	}
+	return values
+}
+
+// Generates the query string.
+func (qb *Builder) GenerateQuery() (string, error) {
+	var qry string
+	var err error
+	switch qb.queryType {
+	case selectQry:
+		qry, err = qb.generateSelectQry()
+	case insertQry:
+		qry, err = qb.generateInsertQry()
+	case updateQry:
+		qry, err = qb.generateUpdateQry()
+	case deleteQry:
+		qry, err = qb.generateDeleteQry()
+	}
+	return qry, err
+}
+
+func (qb *Builder) generateSelectQry() (string, error) {
+	qry, err := qb.generateSelectClause()
+	if err != nil {
+		return "", err
+	}
+	qry += qb.generateFromAndJoinClause()
+	whereClause, err := qb.generateWhereClause()
+	if err != nil {
+		return "", err
+	}
+	qry += whereClause
+	qry += qb.generateOrderByClause()
+	qry += qb.generateLimitClause()
+	return qry, err
+}
+
+func (qb *Builder) generateDeleteQry() (string, error) {
+	qry := qb.generateDeleteClause()
+	qry += qb.generateFromAndJoinClause()
+	whereClause, err := qb.generateWhereClause()
+	if err != nil {
+		return "", err
+	}
+	qry += whereClause
+	return qry, err
+}
+
+func (qb *Builder) generateUpdateQry() (string, error) {
+	qry, err := qb.generateUpdateClause()
+	if err != nil {
+		return "", err
+	}
+	whereClause, err := qb.generateWhereClause()
+	if err != nil {
+		return "", err
+	}
+	qry += whereClause
+	return qry, err
+}
+
+func (qb *Builder) generateInsertQry() (string, error) {
+	qry, err := qb.generateInsertClause()
+	if err != nil {
+		return "", err
+	}
+	return qry, nil
+}
+
+// Generates the SELECT clause. Will return error if the number of values is not equal
+// to the number of columns
+func (qb *Builder) generateSelectClause() (string, error) {
+	if len(qb.columns) != len(qb.values) {
+		return "", NewBadColumnsValuesComboError(len(qb.columns), len(qb.values))
+	}
+	qry := "SELECT "
+	for i, column := range qb.columns {
+		qry += column
+		if i < len(qb.columns)-1 {
+			qry += ","
+		}
+	}
+	return qry, nil
+}
+
+// Generates the join clause
+func (qb *Builder) generateFromAndJoinClause() string {
+	qry := " FROM " + qb.table
+	for _, joinTable := range qb.joinTables {
+		qry += " " + joinTable.joinType +
+			" JOIN " +
+			joinTable.table +
+			" ON " +
+			joinTable.column +
+			"=" +
+			joinTable.fkey
+	}
+	return qry
+}
+
+// Generates the WHERE clause. Will return error if a comparison operator is invalid
+func (qb *Builder) generateWhereClause() (string, error) {
+	if len(qb.criteria) == 0 {
+		return "", nil
+	}
+	qry := " WHERE "
+	for ci, criterion := range qb.criteria {
+		if ci == 0 && criterion.or {
+			return "", ErrFirstCriterionIsOr
+		}
+		if ci != 0 && ci < len(qb.criteria) {
+			switch criterion.or {
+			case true:
+				qry += " OR "
+			default:
+				qry += " AND "
+			}
+		}
+		if !qb.operatorIsValid(criterion.operator) {
+			return "", NewInvalidOperatorError(criterion.operator)
+		}
+		qry += criterion.column
+		if criterion.operator == "BETWEEN" || criterion.operator == "IN" || criterion.operator == "LIKE" {
+			qry += " "
+		}
+		qry += criterion.operator
+		switch {
+		case criterion.operator == "LIKE":
+			qry += " " + qb.addPlaceholder()
+		case criterion.operator == "BETWEEN":
+			qry += " " + qb.addPlaceholder() + " AND " + qb.addPlaceholder()
+		case criterion.operator == "IN":
+			qry += " (" + qb.addPlaceholder() + strings.Repeat(","+qb.addPlaceholder(), len(criterion.values)-1) + ")"
+		default:
+			qry += qb.addPlaceholder()
+		}
+	}
+	return qry, nil
+}
+
+// Generates the ORDER BY clause
+func (qb *Builder) generateOrderByClause() string {
+	if len(qb.orderBy) == 0 {
+		return ""
+	}
+	qry := " ORDER BY "
+	for ci, order := range qb.orderBy {
+		qry += order.column
+		switch {
+		case order.direction == -1:
+			qry += " DESC"
+		default:
+			qry += " ASC"
+		}
+		if ci < len(qb.orderBy)-1 {
+			qry += ","
+		}
+	}
+	return qry
+}
+
+func (qb *Builder) generateLimitClause() string {
+	if qb.limit == 0 {
+		return ""
+	}
+	qry := fmt.Sprintf(" LIMIT %d", qb.limit)
+	if qb.offset > 0 {
+		qry += fmt.Sprintf(",%d", qb.offset)
+	}
+	return qry
+}
+
+// Checks if a comparison operator is valid
+func (qb *Builder) operatorIsValid(operator string) bool {
+	for _, o := range strings.Split(validOperators, "/") {
+		if operator == o {
+			return true
+		}
+	}
+	return false
+}
+
+func NewInsert(tableName string) *Builder {
+	return &Builder{
+		queryType: insertQry,
+		table:     tableName,
+	}
+}
+
+func NewUpdate(tableName string) *Builder {
+	return &Builder{
+		queryType: updateQry,
+		table:     tableName,
+	}
+}
+
+func NewDelete(tableName string) *Builder {
+	return &Builder{
+		queryType: deleteQry,
+		table:     tableName,
+	}
+}
+
+func (qb *Builder) generateInsertClause() (string, error) {
+	if len(qb.columns) != len(qb.values) {
+		return "", NewBadColumnsValuesComboError(len(qb.columns), len(qb.values))
+	}
+	qry := "INSERT INTO " + qb.table + " ("
+	for i, column := range qb.columns {
+		qry += column
+		if i < len(qb.columns)-1 {
+			qry += ","
+		}
+	}
+	qry += ") VALUES ("
+	for i := range qb.values {
+		qry += qb.addPlaceholder()
+		if i < len(qb.values)-1 {
+			qry += ","
+		}
+	}
+	qry += ")"
+	return qry, nil
+}
+
+func (qb *Builder) generateUpdateClause() (string, error) {
+	if len(qb.columns) != len(qb.values) {
+		return "", NewBadColumnsValuesComboError(len(qb.columns), len(qb.values))
+	}
+	qry := "UPDATE " + qb.table + " SET "
+	for i, column := range qb.columns {
+		qry += column + "=" + qb.addPlaceholder()
+		if i < len(qb.columns)-1 {
+			qry += ","
+		}
+	}
+	return qry, nil
+}
+
+func (qb *Builder) generateDeleteClause() string {
+	qry := "DELETE"
+	return qry
+}
+
+func (qb *Builder) addPlaceholder() string {
+	qb.placeholderCount += 1
+	switch qb.db {
+	case POSTGRES:
+		return fmt.Sprintf("$%d", qb.placeholderCount)
+	case ORACLE:
+		return fmt.Sprintf(":%d", qb.placeholderCount)
+	default:
+		return "?"
+	}
+}
---
+++ sqlquerybob_test.go
@@ -0,0 +1,434 @@
+package sqlquerybob
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestItCreatesASimpleSQLStatementWithNoCriteria(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4)
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1"
+	assert.Equal(expected, qry)
+	assert.Equal([]any{&d.field1, &d.field2, &d.field3, &d.field4}, qb.Values())
+}
+
+func TestItReturnsErrorIfColumnsCountNotEqualToValuesCount(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4)
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	assert.ErrorIs(err, err.(ErrBadColumnsValuesCombo))
+	assert.NotNil(err)
+	assert.Equal("", qry)
+}
+
+func TestItCreatesASimpleSQLStatementWithOrder(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		OrderBy("table1.field1").
+		OrderByDescending("table1.field2")
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1" +
+		" ORDER BY table1.field1 ASC,table1.field2 DESC"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{&d.field1, &d.field2, &d.field3, &d.field4}, qb.Values())
+}
+
+func TestItCreatesASimpleSQLStatement(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		Where("table1.field1", "=", "value1")
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1" +
+		" WHERE table1.field1=?"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{&d.field1, &d.field2, &d.field3, &d.field4}, qb.Values())
+	assert.Equal([]any{"value1"}, qb.Criteria())
+}
+
+func TestItCreatesASimpleSQLStatementWithLimit(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		Where("table1.field1", "=", "value1").
+		Limit(10, 0)
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1" +
+		" WHERE table1.field1=?" +
+		" LIMIT 10"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{&d.field1, &d.field2, &d.field3, &d.field4}, qb.Values())
+	assert.Equal([]any{"value1"}, qb.Criteria())
+}
+
+func TestItCreatesASimpleSQLStatementWithLimitAndOffset(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		Where("table1.field1", "=", "value1").
+		Limit(10, 50)
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1" +
+		" WHERE table1.field1=?" +
+		" LIMIT 10,50"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{&d.field1, &d.field2, &d.field3, &d.field4}, qb.Values())
+	assert.Equal([]any{"value1"}, qb.Criteria())
+}
+
+func TestItCreatesASimpleSQLStatementWithAND(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		Where("table1.field1", "=", "value1").
+		Where("table1.field2", "=", "value2")
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1" +
+		" WHERE table1.field1=?" +
+		" AND table1.field2=?"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{&d.field1, &d.field2, &d.field3, &d.field4}, qb.Values())
+	assert.Equal([]any{"value1", "value2"}, qb.Criteria())
+}
+
+func TestItCreatesASimpleSQLStatementWithOR(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		Where("table1.field1", "=", "value1").
+		OrWhere("table1.field2", "=", "value2")
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1" +
+		" WHERE table1.field1=?" +
+		" OR table1.field2=?"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{&d.field1, &d.field2, &d.field3, &d.field4}, qb.Values())
+	assert.Equal([]any{"value1", "value2"}, qb.Criteria())
+}
+
+func TestItReturnsAnErrorIfAWhereOperatorIsInvalid(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		Where("table1.field1", "=", "value1").
+		Where("table1.field2", "ins", 1, 2, 3)
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	assert.ErrorIs(err, err.(ErrInvalidSqlOperator))
+	assert.Equal("", qry)
+}
+
+func TestItReturnsAnErrorIfTheFirstCriterionIsAnOR(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		OrWhere("table1.field1", "=", "value1")
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	assert.ErrorIs(err, ErrFirstCriterionIsOr)
+	assert.Equal("", qry)
+}
+
+func TestItCreatesAnSQLStatementWithINoperator(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	criteria := []any{1, 2, 3}
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		Where("table1.field2", "in", criteria...)
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1" +
+		" WHERE table1.field2 IN (?,?,?)"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+}
+
+func TestItCreatesAnSQLStatementWithLIKEoperator(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+	}
+	var d dataStruct
+	criteria := []any{"test"}
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4).
+		Where("table1.field2", "like", criteria...)
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4" +
+		" FROM table1" +
+		" WHERE table1.field2 LIKE ?"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+}
+
+func TestItCreatesAnSQLStatementWithJoins(t *testing.T) {
+	type dataStruct struct {
+		field1 string
+		field2 int
+		field3 int
+		field4 string
+		field5 string
+		field6 string
+	}
+	var d dataStruct
+	qb := NewSelect("table1").
+		Select("field1", "field2", "field3", "field4", "table2.field5", "table3.field6").
+		Into(&d.field1, &d.field2, &d.field3, &d.field4, &d.field5, &d.field6).
+		Join("LEFT", "table2", "table2.table1_id", "table1.id").
+		Join("LEFT", "table3", "table3.table1_id", "table1.id").
+		Where("table1.field1", "=", "value1").
+		Where("table1.field2", "IN", 1, 2, 3, 4).
+		Where("table1.field3", "BETWEEN", 1, 10)
+
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "SELECT table1.field1,table1.field2,table1.field3,table1.field4,table2.field5,table3.field6" +
+		" FROM table1 LEFT JOIN table2 ON table2.table1_id=table1.id LEFT JOIN table3 ON table3.table1_id=table1.id" +
+		" WHERE table1.field1=? AND table1.field2 IN (?,?,?,?) AND table1.field3 BETWEEN ? AND ?"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{&d.field1, &d.field2, &d.field3, &d.field4, &d.field5, &d.field6}, qb.Values())
+	assert.Equal([]any{"value1", 1, 2, 3, 4, 1, 10}, qb.Criteria())
+}
+
+func TestItCreatesASimpleDeleteStatement(t *testing.T) {
+	qb := NewDelete("table1").Where("table1.field1", "=", "value1")
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "DELETE FROM table1 WHERE table1.field1=?"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{"value1"}, qb.Criteria())
+}
+
+func TestItReturnsAnErrorIfDeleteWhereClauseInvalid(t *testing.T) {
+	qb := NewDelete("table1").Where("table1.field1", "!=", "value1")
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	assert.NotNil(err)
+	assert.Equal("", qry)
+}
+
+func TestItCreatesASimpleUpdateStatement(t *testing.T) {
+	qb := NewUpdate("table1").
+		Set("field1", "field2", "field3", "field4").
+		To("value1", 2, 5, "value4").
+		Where("table1.id", "=", 10)
+
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "UPDATE table1 SET field1=?,field2=?,field3=?,field4=?" +
+		" WHERE table1.id=?"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{"value1", 2, 5, "value4"}, qb.Values())
+	assert.Equal([]any{10}, qb.Criteria())
+}
+
+func TestItReturnsAnErrorIfAnUpdateWhereClauseIsInvalid(t *testing.T) {
+	qb := NewUpdate("table1").
+		Set("field1", "field2", "field3", "field4").
+		To("value1", 2, 5, "value4").
+		Where("table1.id", "!=", 10)
+
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	assert.NotNil(err)
+	assert.Equal("", qry)
+}
+
+func TestItReturnsAnErrorIfUpdateColumnsNotEqualToValues(t *testing.T) {
+	qb := NewUpdate("table1").
+		Set("field1", "field2", "field3", "field4").
+		To("value1", 2, 5).
+		Where("table1.id", "=", 10)
+
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	assert.NotNil(err)
+	assert.Equal("", qry)
+}
+
+func TestItCreatesASimpleInsertStatement(t *testing.T) {
+	qb := NewInsert("table1").
+		Set("field1", "field2", "field3", "field4").
+		To("value1", 2, 5, "value4")
+
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "INSERT INTO table1 (field1,field2,field3,field4) VALUES (?,?,?,?)"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{"value1", 2, 5, "value4"}, qb.Values())
+}
+
+func TestItCreatesASimpleInsertStatementForPostgres(t *testing.T) {
+	qb := NewInsert("table1").
+		ForDatabase(POSTGRES).
+		Set("field1", "field2", "field3", "field4").
+		To("value1", 2, 5, "value4")
+
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "INSERT INTO table1 (field1,field2,field3,field4) VALUES ($1,$2,$3,$4)"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{"value1", 2, 5, "value4"}, qb.Values())
+}
+
+func TestItCreatesASimpleInsertStatementForOracle(t *testing.T) {
+	qb := NewInsert("table1").
+		ForDatabase(ORACLE).
+		Set("field1", "field2", "field3", "field4").
+		To("value1", 2, 5, "value4")
+
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	expected := "INSERT INTO table1 (field1,field2,field3,field4) VALUES (:1,:2,:3,:4)"
+	assert.Nil(err)
+	assert.Equal(expected, qry)
+	assert.Equal([]any{"value1", 2, 5, "value4"}, qb.Values())
+}
+
+func TestItReturnsAnErrorIfInsertColumnsNotEqualToValues(t *testing.T) {
+	qb := NewInsert("table1").
+		Set("field1", "field2", "field3", "field4").
+		To("value1", 2, 5)
+
+	qry, err := qb.GenerateQuery()
+
+	assert := assert.New(t)
+	assert.NotNil(err)
+	assert.Equal("", qry)
+}
